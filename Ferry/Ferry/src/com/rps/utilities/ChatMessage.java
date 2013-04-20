e2fa38bb8df5dfc1f9ca674217382ff572f207a4
Archana Shree VS <archanas0905@gmail.com>
2013-04-20 00:30:47 -0400

    missing

---
+++ Ferry/Ferry/src/com/rps/utilities/ChatMessage.java
@@ -0,0 +1,41 @@
+package com.rps.utilities;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+@SuppressWarnings("serial")
+public class ChatMessage implements Serializable {
+	private ChatMessageTypes messageType;
+	private Object content;
+	private Date timestamp;
+
+	public ChatMessage(ChatMessageTypes messageType,
+			Object content,Date timestamp) {
+		this.messageType = messageType;
+		this.content = content;
+		this.timestamp = timestamp;
+	}
+
+	public ChatMessageTypes getMessageType() {
+		return messageType;
+	}
+
+	public void setMessageType(ChatMessageTypes messageType) {
+		this.messageType = messageType;
+	}
+
+	public Object getContent() {
+		return content;
+	}
+
+	public void setContent(Object content) {
+		this.content = content;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+}
